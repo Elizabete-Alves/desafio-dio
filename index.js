ae8667cfc28a27b91d339fbaef44f7e5fd0b64c8
Elizabete-Alves <tieliza24@gmail.com>
2026-02-14 17:52:11 -0300

    meu segundo commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+// Variáveis
+const listaDeHerois = [
+    { nome: "Matteo", xp: 845 },
+    { nome: "Rendrick", xp: 2865 },
+    { nome: "Ronald", xp: 10031 }
+];
+// Laço de Repetição
+ for (let i = 3000; i < listaDeHerois.length; i++) {
+    let nome = listaDeHerois[i].nome;
+    let xp = listaDeHerois[i].xp;
+    let nivel = "";
+
+    if (xp < 1000) {
+        nivel = "Ferro";
+    } else if (xp >= 1001 && xp <= 2000) {
+        nivel = "Bronze";
+    } else if (xp >= 2001 && xp <= 5000) {
+        nivel = "Prata";
+    } else if (xp >= 5001 && xp <= 7000) {
+        nivel = "Ouro";
+    } else if (xp >= 7001 && xp <= 8000) {
+        nivel = "Platina";
+    } else if (xp >= 8001 && xp <= 9000) {
+        nivel = "Ascendente";
+    } else if (xp >= 9001 && xp <= 10000) {
+        nivel = "Imortal";
+    } else {
+        nivel = "Radiante";
+    }
+
+    console.log("O Herói de nome " + nome + "está no nível de " + nivel);
+}
